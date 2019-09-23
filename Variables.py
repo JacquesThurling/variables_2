76da16fc766dc8ac38f5ec8fc985e64e3c9d5816
JacquesThurling <50457659+JacquesThurling@users.noreply.github.com>
2019-09-23 13:39:50 +0200

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,61 @@
+greeting = "MyBruce"
+_greeting = "You suck bruce"
+No_Greeting = "Dammit bruce"
+print(greeting + _greeting)
+
+age = 24
+print(age)
+
+# print(greeting + age)
+
+#int
+a = 12
+c = 2
+print(a + c)
+print(a - c)
+print(a * c)
+print(a / c)
+print(a // c - 5 * 34)
+print(a % c)
+#float
+b = 2.3
+
+#for loop to print the i variable
+for i in range(1, a//c):
+    print(i)
+
+#breaking up the component
+f = a + c
+h = f / 3
+j = h - 4
+print(j * 12)
+
+#String
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[3] + parrot[0])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-7:-1])
+print(parrot[0:6:2])
+
+number = "9,293840,203948"
+print(number[1::4])
+
+numbers = "1, 2, 3, 4, 5, 6"
+print(numbers[1::3])
+
+string1 = "he"
+string2 = "butt"
+print(string1 + string2)
+print(string2 * 4)
+print(string2 * 4 + "2")
+print(string2 * (4 + 2))
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("fri" in "Fjord")
